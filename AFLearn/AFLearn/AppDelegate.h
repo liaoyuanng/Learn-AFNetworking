0a07c9f44d2bd2708be62a596f811da5bf5ef8c5
JuLiaoyuan <stu0615@163.com>
2018-02-28 16:04:35 +0800

    add annotation

---
+++ AFLearn/AFLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFLearn
+//
+//  Created by Ju Liaoyuan on 2018/1/24.
+//  Copyright © 2018年 Fangzhiwei Information Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFLearn/AFLearn/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  AFLearn
+//
+//  Created by Ju Liaoyuan on 2018/1/24.
+//  Copyright © 2018年 Fangzhiwei Information Technology Co., Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AFNetworking.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // 主要是在阅读中做了一些自己理解的注释。具体的可以 AFN 看每个类的 .m 文件
+}
+
+
+@end
